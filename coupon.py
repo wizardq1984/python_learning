6f4a6868e8e7dcb45a94c82a4fda04d2ab19ab76
wizardq1984 <47135486+wizardq1984@users.noreply.github.com>
2019-02-01 19:11:15 +0800

    Coupon

---
+++ coupon.py
@@ -0,0 +1,18 @@
+import random
+import string
+def coupletter():
+    letter_list = [x for x in string.ascii_letters]
+    random.shuffle(letter_list)
+    coup = ''.join(letter_list[:8])
+    return coup
+
+couplist = []
+while len(couplist) < 200:
+    coup1 = coupletter()
+    if coup1 in couplist:
+        coup1 = coupletter()
+    else:
+        couplist.append(coup1)
+print(couplist)
+print('coupon number is:', len(couplist))
+
