37eda13f1c50cbf428c20a68f9fd7c7f7cb66fe9
wizardq1984 <47135486+wizardq1984@users.noreply.github.com>
2019-02-14 15:53:37 +0800

    can ignore capital letters

--- block_word.py
+++ block_word.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import re
 # get key words list from file without useless blank space
 with open('E:/Python_folder/words.txt') as words:
@@ -18,12 +19,20 @@ def word_replace(word):
     return replaced
 
 
+# identify word is pure eng or not
+def pure_eng(word):
+    return all(ord(c) < 128 for c in word)
+
+
 # split sentence include punctuation
 user_input = re.split(r'(\w+)', input('Please input a sentence:'))
+# print(user_input)
 for j in range(0, len(user_input)):
-    if user_input[j] in key_words:
-        user_input[j] = word_replace(user_input[j])
-        replaced_word = ''.join(user_input)
+    if pure_eng(user_input[j]):
+        if user_input[j].lower() in key_words:
+            user_input[j] = word_replace(user_input[j])
     else:
-        replaced_word = ''.join(user_input)
+        if user_input[j] in key_words:
+            user_input[j] = word_replace(user_input[j])
+    replaced_word = ''.join(user_input)
 print(replaced_word)
