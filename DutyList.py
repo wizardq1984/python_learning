7579d69936d3ac95143c140a6b5eee8ece0dae3e
JasonJQ <47135486+wizardq1984@users.noreply.github.com>
2022-02-19 19:02:27 +0800

    Update DutyList.py

--- DutyList.py
+++ DutyList.py
@@ -3,9 +3,13 @@ import numpy as np
 from datetime import datetime
 
 
-def data_list(beginDate, endDate):
-    date_l = [datetime.strftime(x, '%Y-%m-%d') for x in list(pd.date_range(start=beginDate, end=endDate))]
+def data_list(begin_Date, end_Date):
+    date_l = [datetime.strftime(x, '%Y-%m-%d') for x in list(pd.date_range(start=begin_Date, end=end_Date))]
     return date_l
+
+def duty_list(day_num, start_type):
+    duty_l =
+    return duty_l
 
 
 type_1 = ['A', 'B', 'R', 'R', 'B', 'A', 'C']
@@ -15,5 +19,15 @@ type_4 = ['B', 'C', 'B', 'A', 'B', 'R', 'R']
 type_5 = ['B', 'B', 'A', 'B', 'C', 'R', 'R']
 type_6 = ['B', 'B', 'C', 'B', 'A', 'R', 'R']
 
-Rule_list = ['1', '5', '3', '2', '6', '4']
-print(type(type_1), type(Rule_list))
+Rule_list = []
+Rule_list.extend(type_1)
+Rule_list.extend(type_5)
+Rule_list.extend(type_3)
+Rule_list.extend(type_2)
+Rule_list.extend(type_6)
+Rule_list.extend(type_4)
+
+User_List = ['Patrick', 'Black', 'Amber', 'Jingyu', 'Sam', 'Jason']
+
+print(Rule_list)
+print(len(Rule_list))
